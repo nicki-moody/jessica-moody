4a46649ccdbcf3d4f55cad30932b517f48c0a27b
Nicki Moody <nicki@re-echo.co.uk>
2023-05-10 13:25:38 +0800

    Fixing paths

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,9 @@ class Footer extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-            <a target="_blank" href="https://www.linkedin.com/in/dr-jessica-moody-11085944"><img src="images/linkedin.svg" alt="Linked In Icon"/></a>
-            <a target="_blank" href="https://twitter.com/JessMoody89"><img src="images/twitter.svg" alt="Twitter Icon"/></a>
-            <a target="_blank" href="mailto:jessmoody475@gmail.com"><img src="images/email.png" alt="Email Icon"/></a>
+            <a target="_blank" href="https://www.linkedin.com/in/dr-jessica-moody-11085944"><img src="/images/linkedin.svg" alt="Linked In Icon"/></a>
+            <a target="_blank" href="https://twitter.com/JessMoody89"><img src="/images/twitter.svg" alt="Twitter Icon"/></a>
+            <a target="_blank" href="mailto:jessmoody475@gmail.com"><img src="/images/email.png" alt="Email Icon"/></a>
             <span class="icons-link">Icons by <a target="_blank" href="https://icons8.com" class="fdm__copy-html-content fdm__copy-html-content-link">Icons8</a></span>
         `;
     }   
